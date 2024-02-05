cae02626a65966cc803e106302aadd8356bfa783
brenodanyel <bdls1704@hotmail.com>
2024-02-05 14:37:24 -0300

    add settings module and rabbitmq connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { SettingsModule } from './infrastructure/settings/settings.module';
 import { UsersModule } from './infrastructure/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [SettingsModule, UsersModule],
   controllers: [AppController],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { SettingsService } from './infrastructure/settings/settings.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const settingsService = app.get(SettingsService);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [settingsService.getRabbitMQUrl()],
+      queue: 'user_queue',
+      queueOptions: {},
+    },
+  });
+
   app.setGlobalPrefix('/api');
 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
